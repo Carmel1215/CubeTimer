90171a6681aab3b4a3ad414ba11bf8523a4d07b3
Carmel1215 <kth12153@gmail.com>
2026-01-03 23:01:05 +0900

    Initial Commit

---
+++ CubeTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CubeTimer
+//
+//  Created by 김태환 on 1/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CubeTimer/CubeTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubeTimerApp.swift
+//  CubeTimer
+//
+//  Created by 김태환 on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct CubeTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
