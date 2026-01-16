acf0ed4571feb5ca92220a95fd5f63acde7e3636
Carmel1215 <kth12153@gmail.com>
2026-01-16 17:29:32 +0900

    refactor: Timer 상태 관리 위치 변경
    TimerScreen -> TimerStore로 변경

--- CubeTimer/Model/Stores/TimerStore.swift
+++ CubeTimer/Model/Stores/TimerStore.swift
@@ -6,9 +6,17 @@
 //
 
 import Foundation
-import SwiftData
+import Combine
 
-final class TimerStore {
+enum TimerState {
+    case ready
+    case running
+    case stopped
+}
+
+final class TimerStore: ObservableObject {
+    
+    @Published var timerState: TimerState = .stopped
     // TODO: 타이머 시간 측정 로직
     // TODO: SwiftData 사용법 알아보기
 }
--- CubeTimer/View/Screens/TimerScreen.swift
+++ CubeTimer/View/Screens/TimerScreen.swift
@@ -8,15 +8,8 @@
 import SwiftUI
 
 struct TimerScreen: View {
+    @StateObject private var store: TimerStore = TimerStore()
     
-    enum TimerState {
-        case ready
-        case running
-        case stopped
-    }
-    
-    @State private var timerState: TimerState = .stopped
-
     @GestureState private var isTouching = false
 
     var body: some View {
@@ -25,22 +18,22 @@ struct TimerScreen: View {
             Text("00:00:00")
                 .font(.system(size: 64, design: .monospaced))
                 .bold()
-                .foregroundStyle(timerState == .running ? .white : .black)
+                .foregroundStyle(store.timerState == .running ? .white : .black)
         }
         .contentShape(Rectangle())
         .highPriorityGesture(
             TapGesture()
                 .onEnded {
-                    if timerState == .running {
-                        timerState = .stopped
+                    if store.timerState == .running {
+                        store.timerState = .stopped
                     }
                 }
         )
         .gesture(
             LongPressGesture(minimumDuration: 0.5)
                 .onEnded { _ in
-                    if timerState == .stopped {
-                        timerState = .ready
+                    if store.timerState == .stopped {
+                        store.timerState = .ready
                         let generator = UIImpactFeedbackGenerator(style: .medium)
                         generator.prepare()
                         generator.impactOccurred()
@@ -53,15 +46,15 @@ struct TimerScreen: View {
                     state = true // 터치 중
                 }
                 .onEnded { _ in
-                    if timerState == .ready {
-                        timerState = .running
+                    if store.timerState == .ready {
+                        store.timerState = .running
                     }
                 }
         )
     }
 
     var backgroundColor: Color {
-        switch timerState {
+        switch store.timerState {
         case .stopped: return .white
         case .ready: return .green
         case .running: return .black
