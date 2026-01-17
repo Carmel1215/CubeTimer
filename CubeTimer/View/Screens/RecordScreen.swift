3ab95b25e80cd10624b67e6e04232f7eadec491a
Carmel1215 <kth12153@gmail.com>
2026-01-17 21:59:15 +0900

    feat: Record 삭제 기능 추가

--- CubeTimer/View/Screens/RecordScreen.swift
+++ CubeTimer/View/Screens/RecordScreen.swift
@@ -9,23 +9,38 @@ import SwiftUI
 import SwiftData
 
 struct RecordScreen: View {
+    @Environment(\.modelContext) private var modelContext
+    
     @Query(sort: \Record.createdAt, order: .reverse)
     private var records: [Record]
     
     var body: some View {
         NavigationStack {
-            List(records) { r in
-                HStack {
-                    Text("\(r.time)")
-                        .font(.headline)
-                    
-                    Spacer()
-                    
-                    Text(r.createdAt, style: .date)
-                        .foregroundStyle(.secondary)
+            List {
+                ForEach(records, id: \.id) { r in
+                    HStack {
+                        Text("\(r.time)")
+                            .font(.headline)
+                        
+                        Spacer()
+                        
+                        Text(r.createdAt, style: .date)
+                            .foregroundStyle(.secondary)
+                    }
                 }
+                .onDelete(perform: delete)
             }
             .navigationTitle("Records")
+            .toolbar {
+                EditButton()
+            }
+            
+        }
+    }
+    
+    private func delete(_ indexSet: IndexSet) {
+        for index in indexSet {
+            modelContext.delete(records[index])
         }
     }
 }
